f87578d1051486d72990c65e0a02d94fcb5341f3
erickperez-hash <erick.perez@pursuit.org>
2025-09-20 11:19:50 -0400

    images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Render!
-          </p>
+          <p>My First Render React Deployment!!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
